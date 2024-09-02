fc8f3443b05228128389e8a32382cf3924ac57f0
thepmsquare <impmmmm@gmail.com>
2024-09-02 15:05:58 +0530

    :sparkles: app table

--- square_database_structure/square/public/tables.py
+++ square_database_structure/square/public/tables.py
@@ -17,3 +17,11 @@ class Test(Base):
         Integer, primary_key=True, nullable=False, unique=True, autoincrement=True
     )
     test_text = Column(String, nullable=False)
+
+class App(Base):
+    __tablename__ = "app"
+
+    app_id = Column(
+        Integer, primary_key=True, nullable=False, unique=True, autoincrement=True
+    )
+    app_name = Column(String, nullable=False, unique=True)
