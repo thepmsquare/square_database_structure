4de0a29ffb534016b70266dfd5ce586cea5f0ae7
thepmsquare <impmmmm@gmail.com>
2024-11-18 12:41:49 +0530

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = "square_database_structure"
 
 setup(
     name=package_name,
-    version="1.3.3",
+    version="1.3.4",
     packages=find_packages(),
     package_data={
         package_name: ["**/**/stored_procedures_and_functions/*.sql"],
