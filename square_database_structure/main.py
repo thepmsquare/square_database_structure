584a112c7e7dde5ef9f856a4a9485ae3545f470d
Parth Mangtani <impmmmm@gmail.com>
2025-06-09 07:41:15 +0530

    :building_construction: add email schema integration in main.py

--- square_database_structure/main.py
+++ square_database_structure/main.py
@@ -13,6 +13,18 @@ from square_database_structure.square.authentication.stored_procedures_and_funct
 from square_database_structure.square.authentication.tables import (
     Base as SquareAuthenticationBase,
 )
+from square_database_structure.square.email import (
+    global_string_schema_name as local_string_square_email_schema_name,
+)
+from square_database_structure.square.email.data import (
+    data_to_insert as local_list_square_email_data_to_insert,
+)
+from square_database_structure.square.email.stored_procedures_and_functions import (
+    stored_procedures_and_functions as local_list_square_email_stored_procedures_and_functions,
+)
+from square_database_structure.square.email.tables import (
+    Base as SquareEmailBase,
+)
 from square_database_structure.square.file_storage import (
     global_string_schema_name as local_string_square_file_storage_schema_name,
 )
@@ -77,6 +89,12 @@ global_list_create = [
                 "base": SquareGreetingBase,
                 "data_to_insert": local_list_square_greeting_data_to_insert,
                 "stored_procedures_and_functions": local_list_square_greeting_stored_procedures_and_functions,
+            },
+            {
+                "schema": local_string_square_email_schema_name,
+                "base": SquareEmailBase,
+                "data_to_insert": local_list_square_email_data_to_insert,
+                "stored_procedures_and_functions": local_list_square_email_stored_procedures_and_functions,
             },
         ],
     }
