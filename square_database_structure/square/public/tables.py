aa73ad1de7460f9de11722e650208b78b6965c90
thepmsquare <impmmmm@gmail.com>
2025-07-10 08:34:14 +0530

    Update tables.py

--- square_database_structure/square/public/tables.py
+++ square_database_structure/square/public/tables.py
@@ -1,7 +1,19 @@
-from sqlalchemy import MetaData, Column, Integer, String
 from sqlalchemy.orm import declarative_base
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    DateTime,
+    Boolean,
+    Float,
+    JSON,
+    LargeBinary,
+    Enum,
+    MetaData
+)
 
 from square_database_structure.square.public import global_string_schema_name
+from square_database_structure.square.public.enums import TestEnumEnum
 
 Base = declarative_base(metadata=MetaData(schema=global_string_schema_name))
 
@@ -12,8 +24,13 @@ class Test(Base):
     test_id = Column(
         Integer, primary_key=True, nullable=False, unique=True, autoincrement=True
     )
-    test_text = Column(String, nullable=False, unique=True)
-
+    test_text = Column(String, nullable=True, unique=True)
+    test_datetime = Column(DateTime, nullable=True)
+    test_bool = Column(Boolean, nullable=True)
+    test_enum_enum = Column(Enum(TestEnumEnum), nullable=True)
+    test_float = Column(Float, nullable=True)
+    test_json = Column(JSON, nullable=True)
+    test_blob = Column(LargeBinary, nullable=True)
 
 class App(Base):
     __tablename__ = "app"
